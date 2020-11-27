acec32a873b8146a81d7512dfd30de7a08e18c32
Diego Baron <diego.baron@cern.ch>
2020-11-27 21:16:11 +0000

    Analysis without ZpT rewighted and the best cuts to mimic Z->TauTau kinematics, changes to the style of the Zll plots as well

--- Zee/MC/Analysis_LastRunned.C
+++ Zee/MC/Analysis_LastRunned.C
@@ -279,10 +279,10 @@ void CLoop::Fill(double weight, int z_sample) {
         // Cuts bits
         vector<int> cuts={0,0,0,0,0,0,0};
         int random=rand()%2;
-        double a{60},b{35};
+        double a{55},b{35};
         if(random){
           a=35;
-          b=60;
+          b=55;
         }
         if (angle<=11.5*pi/18){
           cuts[0]=1;
@@ -303,11 +303,11 @@ void CLoop::Fill(double weight, int z_sample) {
           cuts[5]=1;
         }
         if(random){
-          if(elec_1_p4->Pt()>=(b+15)){
+          if(elec_1_p4->Pt()>=(b+20)){
             cuts[6]=1;
           }
         } else{
-          if(elec_0_p4->Pt()>=(a+15)){
+          if(elec_0_p4->Pt()>=(a+20)){
             cuts[6]=1;
           }
         }
