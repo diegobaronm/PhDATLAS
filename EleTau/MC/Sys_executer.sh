cee8ff7e97964b555f86ad3c78ec069d4aa5d707
Diego Baron <diego.baron@cern.ch>
2021-07-26 20:25:09 +0100

    Started analysing systematic ntuples.

--- EleTau/MC/Sys_executer.sh
+++ EleTau/MC/Sys_executer.sh
@@ -11,7 +11,7 @@ if [ "$2" = "NOMINAL" ]
 then
 	parallel -j $1 --progress -a samples_sys.txt python3 RunAnalysis.py ::: no ::: $2
 	hadd out/Signal_Sherpa$2.root out/Ztautau_sherpa*_sys_*[0-9].root
-	hadd out/Signal_PoPy$2.root out/Ztautau_201_sys_*[0-9].root
+	hadd out/Signal_PoPy$2.root out/Ztautau_201*_sys_*[0-9].root
 	hadd out/Zjets$2.root out/Zmumu_*_sys_*[0-9].root out/Zee_*_sys_*[0-9].root
 	hadd out/VV$2.root out/llll*_sys_*[0-9].root out/lllv*_sys_*[0-9].root out/llvv*_sys_*[0-9].root out/lvvv*_sys_*[0-9].root out/ZqqZvv*_sys_*[0-9].root out/ZqqZll*_sys_*[0-9].root out/WqqZvv*_sys_*[0-9].root out/WqqZll*_sys_*[0-9].root out/WlvZqq*_sys_*[0-9].root
 fi
