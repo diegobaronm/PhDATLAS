4bdbf1aed368c4f40d4e137bf7954269111db7d4
Diego Baron <diego.baron@cern.ch>
2021-08-15 14:54:04 +0100

    Pushing merging script fixes.

--- TauMu/MC/sys_merger.py
+++ TauMu/MC/sys_merger.py
@@ -6,7 +6,7 @@ samples_dir=sys.argv[1]
 os.chdir(samples_dir)
 
 for d in os.lisdir():
-	os.system("hadd out/Signal_Sherpa"+t+".root "+d+"/Ztautau_sherpa*_sys_*"+t+".root")
-	os.system("hadd out/Signal_PoPy"+t+".root "+d+"/Ztautau_201*_sys_*"+t+".root")
-	os.system("hadd out/Zmumu"+t+".root "+d+"/Zmumu_201*_sys_*"+t+".root")
-	os.system("hadd out/VV"+t+".root "+d+"/llll*_sys_*"+t+".root "+d+"/lllv*_sys_*"+t+".root "+d+"/llvv*_sys_*"+t+".root "+d+"/lvvv*_sys_*"+t+".root "+d+"/ZqqZvv*_sys_*"+t+".root "+d+"/ZqqZll*_sys_*"+t+".root "+d+"/WqqZvv*_sys_*"+t+".root "+d+"/WqqZll*_sys_*"+t+".root "+d+"/WlvZqq*_sys_*"+t+".root")
+	os.system("hadd out/Signal_Sherpa"+d+".root "+d+"/Ztautau_sherpa*_sys_*"+d+".root")
+	os.system("hadd out/Signal_PoPy"+d+".root "+d+"/Ztautau_201*_sys_*"+d+".root")
+	os.system("hadd out/Zmumu"+d+".root "+d+"/Zmumu_201*_sys_*"+d+".root")
+	os.system("hadd out/VV"+d+".root "+d+"/llll*_sys_*"+d+".root "+d+"/lllv*_sys_*"+d+".root "+d+"/llvv*_sys_*"+d+".root "+d+"/lvvv*_sys_*"+d+".root "+d+"/ZqqZvv*_sys_*"+d+".root "+d+"/ZqqZll*_sys_*"+d+".root "+d+"/WqqZvv*_sys_*"+d+".root "+d+"/WqqZll*_sys_*"+d+".root "+d+"/WlvZqq*_sys_*"+d+".root")
