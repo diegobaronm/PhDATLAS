8224cd774e5b60032e5a76af15be6341a249df80
Diego Baron <diego.baron@cern.ch>
2021-08-15 14:49:49 +0100

    Pushing merging script.

--- TauMu/MC/sys_merger.py
+++ TauMu/MC/sys_merger.py
@@ -1,15 +1,12 @@
 import os
 import sys
 
-trees=[]
-with open(sys.argv[1],"r") as file:
-	line=file.readline()
-	while line:
-		trees.append(line.rstrip('\n'))
-		line=file.readline()
+samples_dir=sys.argv[1]
 
-for t in trees:
-	os.system("hadd out/Signal_Sherpa"+t+".root out/Ztautau_sherpa*_sys_*"+t+".root")
-	os.system("hadd out/Signal_PoPy"+t+".root out/Ztautau_201*_sys_*"+t+".root")
-	os.system("hadd out/Zmumu"+t+".root out/Zmumu_201*_sys_*"+t+".root")
-	os.system("hadd out/VV"+t+".root out/llll*_sys_*"+t+".root out/lllv*_sys_*"+t+".root out/llvv*_sys_*"+t+".root out/lvvv*_sys_*"+t+".root out/ZqqZvv*_sys_*"+t+".root out/ZqqZll*_sys_*"+t+".root out/WqqZvv*_sys_*"+t+".root out/WqqZll*_sys_*"+t+".root out/WlvZqq*_sys_*"+t+".root")
+os.chdir(samples_dir)
+
+for d in os.lisdir():
+	os.system("hadd out/Signal_Sherpa"+t+".root "+d+"/Ztautau_sherpa*_sys_*"+t+".root")
+	os.system("hadd out/Signal_PoPy"+t+".root "+d+"/Ztautau_201*_sys_*"+t+".root")
+	os.system("hadd out/Zmumu"+t+".root "+d+"/Zmumu_201*_sys_*"+t+".root")
+	os.system("hadd out/VV"+t+".root "+d+"/llll*_sys_*"+t+".root "+d+"/lllv*_sys_*"+t+".root "+d+"/llvv*_sys_*"+t+".root "+d+"/lvvv*_sys_*"+t+".root "+d+"/ZqqZvv*_sys_*"+t+".root "+d+"/ZqqZll*_sys_*"+t+".root "+d+"/WqqZvv*_sys_*"+t+".root "+d+"/WqqZll*_sys_*"+t+".root "+d+"/WlvZqq*_sys_*"+t+".root")
